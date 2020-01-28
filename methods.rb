fc32266866df1646f9792126ec8a5abe937f0f30
Adewale Orotayo <didymus7007@gmail.com>
2020-01-28 16:14:18 +0100

    fixing linter error-__8

--- methods.rb
+++ methods.rb
@@ -1,13 +1,13 @@
 module Enumerable
   def my_each
-		for i in (0..self.length-1)
-			yield self[i]
+	  for i in (0..self.length-1)
+		  yield self[i]
 		end
 	end
 
   def my_each_with_index
-		for i in (0..self.length-1)
-			yield self[i], i
+	  for i in (0..self.length-1)
+		  yield self[i], i
 		end
 	end
 
