a5e9aad1e409f9d921011e6700ead40b03d4a52f
Adewale Orotayo <didymus7007@gmail.com>
2020-01-29 23:54:52 +0100

    review changes completed

--- methods.rb
+++ methods.rb
@@ -1,63 +1,92 @@
 # rubocop:disable all"
-...
+# ...
 # rubocop:enable all
 
-
 module Enumerable
-  def my_each
+	def my_each
+		return to_enum if !block_given?
+
 	  for i in (0..self.length - 1)
-		  yield self[i]
+		  yield(self[i])
 		end
 	end
 
-  def my_each_with_index
+	def my_each_with_index
+		return to_enum if !block_given?
+
 	  for i in (0..self.length - 1)
-		  yield self[i], i
+		  yield(self[i], i)
 		end
 	end
 
   def my_select
-	  arr = []
-		my_each do |x|
-		  arr << x if yield(x)
-		end
+		arr = []
+		return to_enum if !block_given?
+
+		my_each { |x| arr << x if yield(x) }
 		return arr
 	end
 
-  def my_all?
+  def my_all?(pattern = nil)
 	  result = true
 		if block_given?
 			self.my_each do |x|
 				result = false if !yield(x)
+				break if !result
+			end
+		elsif pattern.class == Regexp
+			self.my_each do |x|
+				result = false if !x.match(pattern)
+				break if !result
+			end
+		elsif pattern
+			self.my_each do |x|
+				result = false if pattern != x
+				break if !result
 			end
 		else
 			for i in self
-				result = true if !i
+				result = false if !i
+				break if !result
 			end
 		end
 		return result
 	end
 
-  def my_any?
+  def my_any?(pattern = nil)
 		result = false
 		if block_given?
 			self.my_each do |x|
 				result = true if yield(x)
+				break if result
+			end
+		elsif pattern.class == Regexp
+			self.my_each do |x|
+				result = true if x.match(pattern)
+				break if result
+			end
+		elsif pattern
+			self.my_each do |x|
+				result = true if x == pattern
+				break if result
 			end
 		else
 			for i in self
 				result = true if i
+				break if result
 			end
 		end
 		return result
 	end
 
-  def my_none?
+  def my_none?(pattern = nil)
 		result = false
 		if block_given?
-			self.my_each do |x|
-				result = true if !yield(x)
-			end
+			self.my_each { |x| result = true if !yield(x) }
+		elsif pattern.class == Regexp
+			self.my_each { |x| result = true if !x.match(pattern) }
+		elsif pattern
+			self.my_each { |x|result = true if x != pattern }
 		else
 			for i in self
 				result = true if !i
@@ -84,26 +113,57 @@ module Enumerable
 		return count
 	end
 
-  def my_map(&pro)
+  def my_map(&my_proc)
 		arr = []
-		self.my_each do |x|
-			if pro.nil?
-				arr << pro.call(x)
-			else
-				arr << yield(x)
-			end
+		return to_enum unless block_given?
+
+		if !my_proc
+			self.my_each { |x| arr << yield(x) }
+		else
+			self.my_each { |x| arr << my_proc.call(x)}
 		end
 		return arr
 	end
 
   def my_inject(*args)
-		init = args.length > 0
-		acc = init ? args[0] : self[0]
-	
-		self.drop(init ? 0 : 1).my_each do |item|
-			acc = yield(acc, item)
-		end
-		return acc
+		check = args.length > 0
+		arr = if self.is_a? Array
+						self
+					elsif self.is_a? Range
+						self.to_a
+					end
+
+		sym = args[0].is_a? Symbol
+		sym1 = args[1].is_a? Symbol
+
+		acc = if args.length == 0 || sym
+						arr[0] 
+					elsif args.length > 0 || sym1
+						args[0]
+					end
+		# acc = check ? args[0] : self[0]
+		puts acc
+
+			if  sym 
+				arr.drop(1).my_each do |x|
+					block = lambda { |memo, val| memo.send(args[0], val) }
+					acc = block.call(acc, x)
+				end
+			elsif check && sym1
+				arr.drop(0).my_each do |x|
+					block = lambda { |memo, val| memo.send(args[1], val) }
+					acc = block.call(acc, x)
+				end
+			elsif check
+				arr.drop(0).my_each { |x| acc = yield(acc, x) }
+			elsif check && block_given?
+				arr.drop(0).my_each { |x| acc = yield(acc, x) }
+			elsif !check && block_given?
+				arr.drop(1).my_each { |x| acc = yield(acc, x) }
+			elsif !check
+				arr.drop(0).my_each { |x| acc = yield(acc, x) }
+			end
+			return acc
 	end
 
 end
@@ -111,3 +171,11 @@ end
 def multiply_els(arr)
 	return arr.my_inject { |product, n| product * n }
 end
+
+
+p [1,2,3,4].my_inject(2,:*)
+p (5..10).my_inject { |sum, n| sum + n }
+p (5..10).my_inject { |sum, n| sum + n }            #=> 45
+p (5..10).my_inject(1) { |product, n| product * n } #=> 151200
+
+p longest = %w{ cat sheep bear }.my_inject { |memo, word| memo.length > word.length ? memo : word }
